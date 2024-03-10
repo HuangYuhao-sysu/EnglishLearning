d226db9629fd2935af7a0cf46928d90ce4cda09d
HuangYuhao-sysu <1823551220@qq.com>
2024-03-10 19:25:37 +0800

    [EnglishLearning]Add speaking.py

---
+++ speaking.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+Sentences = {
+'0304':[
+    "Studies have shown that tea has various benefits for people.",
+    "It makes them more alert, improves their memory and concentration, and offers many other benefits.",
+    "I know some people in their 70s and 80s who are in great health, most of whom are avid tea drinkers."
+    ],
+'0305':[
+    "I really enjoy the mild, breezy weather of spring.",
+    "The gentle spring breeze and mild temperatures make outdoor activities very enjoyable.",
+    "It's a time when everything seems to come alive."
+    ],
+}
+
+print(Sentences['0305'])
