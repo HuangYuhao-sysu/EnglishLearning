5043242bc471eda7f05e1d915c13795f17234a6a
Yuhao Huang <18813946109@163.com>
2024-03-18 20:55:03 +0800

    [EnglishLearning]speaking.py

--- speaking.py
+++ speaking.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python3
 
 Sentences = {
-'0304':[
+'1':[
     "Studies have shown that tea has various benefits for people.",
     "It makes them more alert, improves their memory and concentration, and offers many other benefits.",
     "I know some people in their 70s and 80s who are in great health, most of whom are avid tea drinkers."
     ],
-'0305':[
+'2':[
     "I really enjoy the mild, breezy weather of spring.",
     "The gentle spring breeze and mild temperatures make outdoor activities very enjoyable.",
     "It's a time when everything seems to come alive."
     ],
-'0311':[
+'3':[
     "In our fast-paced world, giving gifts is still a big deal.",
     "Whether it's for birthdays, holidays, or just to show appreciation, it's a tradition that brings people together.",
     "I recently got a potted plant from a friend, which keeps reminding me of her affection and presence in my life."
     ],
-'0312':[
+'4':[
     "I'm aiming to make more time for my family and friends.",
     "Life can often get busy, and it's easy to neglect the people who mean the most to us.",
     "I plan to schedule regular get-togethers and make an effort to stay in touch with my loved ones."
     ],
-'0313':[
+'5':[
     "When it comes to films, I have a pretty diverse taste.",
     "However, I would say that I particularly enjoy two main genres, dramas and science fiction",
     "Both dramas and science fiction provide me with an escape from reality.",
     ],
-'0314':[
+'6':[
     "Introverts often prefer quiet and solitary environments.",
     "They enjoy reflecting on their own ideas, and are often good at tasks that require concentration and focus.",
     "This can be an asset in fields like writing, programming, or research, where deep thinking is key.",
     ],
-'0315':[
+'7':[
     "While artificial intelligence is certainly transforming the job market, it's not going to completely eliminate the human workers.",
     "AI can take over some of the routine work, freeing up humans to focus on more complex and critical tasks.",
     "Many jobs require human qualities like empathy, critical-thinking and problem-solving, which AI is still far from replicating.",
     ],
+'8':[
+    "During the pandemic, I had to adapt to online learning, which presented some challenges.",
+    "Staying focused and motivated while learning remotely is difficult, but I managed to overcome these obstacles.",
+    "I believe online learning will continue to evolve and become more effective in the future.",
+]
 }
 
-print(Sentences['0305'])
+print(Sentences['1'])
