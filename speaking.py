062e4b87c7a7b0ae12921c7bce3debc590a45dc5
Yuhao Huang <18813946109@163.com>
2024-03-25 21:54:23 +0800

    [EnglishLearning]speaking.

--- speaking.py
+++ speaking.py
@@ -1,57 +1,57 @@
 #!/usr/bin/env python3
 
 Sentences = {
-'1':[
+'1 tea':[
     "Studies have shown that tea has various benefits for people.",
     "It makes them more alert, improves their memory and concentration, and offers many other benefits.",
     "I know some people in their 70s and 80s who are in great health, most of whom are avid tea drinkers."
     ],
-'2':[
+'2 spring':[
     "I really enjoy the mild, breezy weather of spring.",
     "The gentle spring breeze and mild temperatures make outdoor activities very enjoyable.",
     "It's a time when everything seems to come alive."
     ],
-'3':[
+'3 gift':[
     "In our fast-paced world, giving gifts is still a big deal.",
     "Whether it's for birthdays, holidays, or just to show appreciation, it's a tradition that brings people together.",
     "I recently got a potted plant from a friend, which keeps reminding me of her affection and presence in my life."
     ],
-'4':[
+'4 family and friends':[
     "I'm aiming to make more time for my family and friends.",
     "Life can often get busy, and it's easy to neglect the people who mean the most to us.",
     "I plan to schedule regular get-togethers and make an effort to stay in touch with my loved ones."
     ],
-'5':[
+'5 films':[
     "When it comes to films, I have a pretty diverse taste.",
     "However, I would say that I particularly enjoy two main genres, dramas and science fiction",
     "Both dramas and science fiction provide me with an escape from reality.",
     ],
-'6':[
+'6 introverts':[
     "Introverts often prefer quiet and solitary environments.",
     "They enjoy reflecting on their own ideas, and are often good at tasks that require concentration and focus.",
     "This can be an asset in fields like writing, programming, or research, where deep thinking is key.",
     ],
-'7':[
+'7 AI':[
     "While artificial intelligence is certainly transforming the job market, it's not going to completely eliminate the human workers.",
     "AI can take over some of the routine work, freeing up humans to focus on more complex and critical tasks.",
     "Many jobs require human qualities like empathy, critical-thinking and problem-solving, which AI is still far from replicating.",
     ],
-'8':[
+'8 pandemic':[
     "During the pandemic, I had to adapt to online learning, which presented some challenges.",
     "Staying focused and motivated while learning remotely is difficult, but I managed to overcome these obstacles.",
     "I believe online learning will continue to evolve and become more effective in the future.",
     ],
-'9':[
+'9 dream':[
     "My dream is to become a published author, sharing my stories and ideas with the world.",
     "I'm working hard to achieve this dream by writing regularly, and seeking feedback from others.",
     "I recently finished my first draft of a novel, and I'm really proud.",
     ],
-'10':[
+'10 social media':[
     "Social media has become a powerful tool for connecting with people from all over the world.",
     "I use social media to share my experiences, learn new things, and connect with like-minded individuals.",
     "However, it's important to use it responsibly, avoiding its potential pitfalls such as addiction and misinformation.",
     ],
-'11':[
+'11 cinema':[
     "Cinema has always fascinated me, it offers a unique way of experiencing stories and emotions.",
     "I remember watching my first film as a child, and being transported into a whole new world.",
     "Cinema is magical because it deeply affects our hearts and minds. Some movies can leave a lasting impression on us.",
@@ -60,6 +60,11 @@ Sentences = {
     "I've learned that perseverance is key to overcoming challenges and achieving my goals.",
     "When faced with difficulties, I remind myself that perseverance will eventually lead to success.",
     "The sense of accomplishment I feel after persevering through a challenge is incredibly rewarding.",
+    ],
+'13':[
+    "I follow a balanced diet, incorporating a variety of fruits, vegetables and whole grains into my meals.",
+    "I enjoy trying new cuisines and exploring different flavors from around the world.",
+    "I believe that healthy eating habits are crucial form maintaining good health and well-being.",
     ],
 }
 
